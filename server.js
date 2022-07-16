3a0cdbaca4572936f30e901c68515671002bf71f
Julio Orihuela Vallejos <31975560+julioorihuela@users.noreply.github.com>
2022-07-16 11:06:06 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,64 +1,36 @@
-const fs = require('fs');
-const express = require('express');
+const express = require('express')
 const app = express();
-const PORT = 8080
+const PORT = 8080;
+const Contenedor = require('./contenedor.js') 
 
-class Contenedor {
-    constructor (archivo) {
-        this.archivo = archivo;
-    }
+let dato = []
+let productos = new Contenedor('productos.txt');
 
-    async getAll() {
-        const fs = require('fs');  
-        try {     
-            const contenido = await fs.promises.readFile(this.archivo, 'utf-8')
-            const productos = JSON.parse(contenido);
-            return productos;            
-        }  catch (err) { 
-            console.log('No existen Productos.');
-            //return null;     
-       }      
-    }
+app.get('/', (req, res) => {
+    res.send(`<h1 style="color:blue;">Bienvenidos al servidor express</h1> `)
+});
 
-    async getById(Number) {
-        try {           
-            const productos = await this.getAll();
-            if (productos != null) {
-                const prod = productos.find( prod => prod.id==Number);
-                return prod;  
-            } else {
-                console.log('No existen Productos.');
-                return null;
-            }                          
-        }  catch (err) { 
-            console.log('No existen Productos.');
-            return null;
-        }       
-    }
-    
-  app.get('/productos', (req, res) => {
-    
+app.get('/productos', (req, res) => {
+   
     productos.getAll().then(allFiles => {
         res.send(`${JSON.stringify(allFiles)}`)
     }).catch(err => {
         res.send(err);
     })
-    
-
 });
 
 app.get('/productoRandom', (req, res) => {
-    
+  
     productos.random().then(getRandomFiles => {
         res.send(`${JSON.stringify(getRandomFiles)}`)
     }).catch(err => {
         res.send(err);
     })
-    
-
 });
 
-  
-}
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
+});
+server.on("error", (error) => console.log(`Error en el servidor ${error}`))
 
 
